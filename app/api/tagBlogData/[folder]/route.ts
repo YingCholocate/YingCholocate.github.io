7b0812f831ada9b96b8514f43b358e33083857e7
YingCholocate <3023794294@qq.com>
2023-10-08 18:17:19 +0800

    fix: update depoly method

--- app/api/tagBlogData/[folder]/route.ts
+++ app/api/tagBlogData/[folder]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import fs from 'fs'
+import * as fs from 'fs'
 type RouteSegment = { params: { folder: string } }
 export async function GET(req: Request, { params }: RouteSegment) {
   // 遍历文件夹获取tag值，路径在public下,build之后是在.next文件夹下的
--- app/api/tagData/route.ts
+++ app/api/tagData/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import fs from 'fs'
+import * as fs from 'fs'
 
 export async function GET() {
   // 遍历文件夹获取tag值，路径在public下,build之后是在.next文件夹下的
