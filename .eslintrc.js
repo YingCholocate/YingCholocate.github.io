9631c4dd73efd6cf06831cb195a6a02a45eaeae0
YingCholocate <3023794294@qq.com>
2023-10-08 23:47:22 +0800

    fix: depoly

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,13 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended',
+    'next',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended'
+  ],
   overrides: [
     {
       env: {
