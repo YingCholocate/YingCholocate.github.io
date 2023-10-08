aee29a6c93c4d6d47d5c230f0b47db4b29ef6e3c
YingCholocate <3023794294@qq.com>
2023-10-08 23:33:40 +0800

    fix: depoly

--- app/api/tagData/route.ts
+++ app/api/tagData/route.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs'
 export async function GET() {
   // 遍历文件夹获取tag值，路径在public下,build之后是在.next文件夹下的
 
-  const files = await fs.promises.readdir(`./public/tags/`)
+  const files = await fs.promises.readdir('./public/tags/')
 
   return NextResponse.json({ app: files })
 }
